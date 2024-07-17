5d228e65e98da4495eebf7e9529a0f138bf75a57
guimatheus92 <guilherme_matheus12@hotmail.com>
2024-07-17 14:06:54 -0300

    feat: v1.3

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
-from flask import Flask
-import os
+from flask import Flask, send_from_directory
 from dotenv import load_dotenv
+import os
 
 def create_app(config_class='config.Config'):
     load_dotenv()  # Load environment variables from .env
@@ -10,5 +10,10 @@ def create_app(config_class='config.Config'):
 
     with app.app_context():
         from . import routes
+
+    # Route to serve the favicon
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
     return app
